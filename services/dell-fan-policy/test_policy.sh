59fa55a4d813c3b78a56a512ec9f71a2f93859d2
Bradley Pizzimenti <bradley@gennakersystems.com>
2026-03-18 09:02:11 -0700

    Align policy tests with current state transitions

--- services/dell-fan-policy/test_policy.sh
+++ services/dell-fan-policy/test_policy.sh
@@ -28,7 +28,7 @@ run_case() {
     local expected="$8"
     local actual
 
-    actual="$(desired_state "$current" "$cpu" "$gpu" "$gpu_w" "$wifi" "$on_ac")"
+    actual="$(desired_state "$current" "$cpu" "$gpu" "$gpu_w" "$wifi" "$on_ac" 0 0 0 "balanced" 0)"
     assert_eq "$name" "$expected" "$actual"
 }
 
@@ -38,11 +38,11 @@ assert_eq "clamp_high" "2" "$(clamp_state 5)"
 
 run_case "battery_idle_stays_off" 0 45 44 5 60 0 0
 run_case "battery_warm_cpu_to_state1" 0 60 44 5 60 0 1
-run_case "battery_hot_gpu_to_state2" 0 50 73 5 60 0 2
-run_case "ac_gpu_power_to_state2" 0 50 50 24 60 1 2
+run_case "battery_hot_gpu_to_state1" 0 50 73 5 60 0 1
+run_case "ac_gpu_power_to_state1" 0 50 50 24 60 1 1
 run_case "state1_holds_without_cooldown" 1 56 55 5 60 0 1
 run_case "state1_drops_when_cool" 1 50 50 5 60 0 0
-run_case "state2_drops_to_state1_when_cooled" 2 60 61 5 60 1 1
+run_case "state2_drops_to_state3_when_cooling" 2 60 61 5 60 1 3
 run_case "wifi_guardrail_forces_max" 0 40 40 5 81 0 2
 run_case "cpu_emergency_forces_max" 0 83 40 5 60 0 2
 
